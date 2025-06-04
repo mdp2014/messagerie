5966272d7f859ea330ad368184bbae5272adb4ef
mdp2014 <marin.depibrac@gmail.com>
2025-06-04 12:03:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const supabaseUrl = 'https://sqnjzcqcmtjhbptjlixe.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIs...'; // Clé raccourcie ici pour lisibilité
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxbmp6Y3FjbXRqaGJwdGpsaXhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MTY1ODcsImV4cCI6MjA2NDA5MjU4N30.lJIsRndHSS95pxJrH726jDaHANTaj_Q14IoZ4JNm-Rg';
 const chatMessages = document.getElementById('chat-messages');
 const messageInput = document.getElementById('message-input');
 const sendButton = document.getElementById('send-button');
