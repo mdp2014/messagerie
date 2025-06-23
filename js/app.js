3e8b19eb2baa76aa6d5456978c10507618f2ffcd
mdp2014 <marin.depibrac@gmail.com>
2025-06-23 20:36:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -38,25 +38,25 @@ async function getUsers() {
 }
 
 // Géolocalisation
-function getGeolocation() {
-  return new Promise((resolve, reject) => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        pos => resolve({
-          latitude: pos.coords.latitude,
-          longitude: pos.coords.longitude
-        }),
-        reject
-      );
-    } else reject(new Error('Non supporté'));
-  });
-}
+//function getGeolocation() {
+//  return new Promise((resolve, reject) => {
+//    if (navigator.geolocation) {
+ //     navigator.geolocation.getCurrentPosition(
+  //      pos => resolve({
+  //        latitude: pos.coords.latitude,
+   //       longitude: pos.coords.longitude
+ //       }),
+  //      reject
+//      );
+//    } else reject(new Error('Non supporté'));
+//  });
+//}
 
-async function getCityFromCoordinates(latitude, longitude) {
-  const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`);
-  const data = await response.json();
-  return data.address.city || data.address.town || data.address.village || 'Inconnue';
-}
+//async function getCityFromCoordinates(latitude, longitude) {
+ // const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`);
+//  const data = await response.json();
+//return data.address.city || data.address.town || data.address.village || 'Inconnue';
+//}
 
 // Envoi message
 async function sendMessage(userId, content) {
